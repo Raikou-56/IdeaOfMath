1fe5438876b608759e0de780e71809bdac9dfb2b
Raikou-56 <raikoukit777@gmail.com>
2025-09-25 21:33:05 +0900

    Update MongoDB config and appsettings

--- Program.cs
+++ Program.cs
@@ -44,12 +44,11 @@ app.UseAuthorization();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
+app.UseStaticFiles();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 DataBaseSetup.ShowUsers();
 

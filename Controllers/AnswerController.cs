24cf9d5ad72e4f0bba675c2cb5dffb3982d2aebb
Raikou-56 <raikoukit777@gmail.com>
2025-10-04 15:43:16 +0900

    Moved filename.txt to new-folder

---
+++ Controllers/AnswerController.cs
@@ -0,0 +1,91 @@
+using MongoDB.Driver;
+using Microsoft.AspNetCore.Mvc;
+using MathSiteProject.Extentions;
+using MathSiteProject.Models;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using MathSiteProject.Repositories;
+using MathSiteProject.Repositories.Data;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace MathSiteProject.Controllers;
+
+public class AnswerController : Controller
+{
+    private readonly AnswerHistoryRepository _answerHistoryRepo;
+
+    public AnswerController(AnswerHistoryRepository answerHistoryRepo)
+    {
+        _answerHistoryRepo = answerHistoryRepo;
+    }
+
+    [HttpPost]
+    public IActionResult SendAnswer(string serial)
+    {
+        ViewBag.Message = serial;
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult LookAnswer(string serial)
+    {
+        ViewBag.Message = serial;
+        List<Problem> problemsData = DataBaseSetup.GetProblems();
+
+        if (int.TryParse(serial.ToString(), out int number))
+        {
+            var match = problemsData.FirstOrDefault(p => p.SerialNumber == number);
+            if (match != null)
+            {
+                var AnswerInf = new AnswerViewModel
+                {
+                    SerialNumber = number,
+                    Problem = match.ProblemLatex,
+                    Answer = match.AnswerLatex,
+                    Teacher = match.Teacher
+                };
+                return View(AnswerInf);
+            }
+            else
+            {
+                var AnswerInf = new AnswerViewModel
+                {
+                    SerialNumber = number,
+                    Problem = "問題が存在しません",
+                    Answer = "解答が存在しません"
+                };
+                return View(AnswerInf);
+            }
+        }
+        else
+        {
+            Console.WriteLine("通し番号の変換に失敗しました");
+            return View("Home", "Index");
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ScoreAnswer(string historyId, int Score)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest("モデルが無効です");
+        }
+        var history = await _answerHistoryRepo.GetByIdAsync(historyId);
+        if (history == null) return NotFound($"ID {historyId} の履歴が見つかりません");
+
+        Console.WriteLine(Score);
+
+        // スコアの保存（ここは設計次第で柔軟に）
+        history.Scoring = true;
+        history.Score = Score; // 合計でも平均でもOK
+        history.IsCorrect = Score == 50; // 仮の判定ロジック
+
+        await _answerHistoryRepo.UpdateAsync(history);
+
+        return RedirectToAction("Index"); // 採点後の遷移先
+    }
+
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -83,51 +83,6 @@ public class HomeController : Controller
         return View(Tmodel);
     }
 
-    [HttpPost]
-    public IActionResult LookAnswer(string serial)
-    {
-        ViewBag.Message = serial;
-        List<Problem> problemsData = DataBaseSetup.GetProblems();
-
-        if (int.TryParse(serial.ToString(), out int number))
-        {
-            var match = problemsData.FirstOrDefault(p => p.SerialNumber == number);
-            if (match != null)
-            {
-                var AnswerInf = new AnswerViewModel
-                {
-                    SerialNumber = number,
-                    Problem = match.ProblemLatex,
-                    Answer = match.AnswerLatex,
-                    Teacher = match.Teacher
-                };
-                return View(AnswerInf);
-            }
-            else
-            {
-                var AnswerInf = new AnswerViewModel
-                {
-                    SerialNumber = number,
-                    Problem = "問題が存在しません",
-                    Answer = "解答が存在しません"
-                };
-                return View(AnswerInf);
-            }
-        }
-        else
-        {
-            Console.WriteLine("通し番号の変換に失敗しました");
-            return View("Home", "Index");
-        }
-    }
-
-    [HttpPost]
-    public IActionResult SendAnswer(string serial)
-    {
-        ViewBag.Message = serial;
-        return View();
-    }
-
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
@@ -197,27 +152,7 @@ public class HomeController : Controller
     }
 
 
-    [HttpPost]
-    public async Task<IActionResult> ScoreAnswer(string historyId, int Score)
-    {
-        if (!ModelState.IsValid)
-        {
-            return BadRequest("モデルが無効です");
-        }
-        var history = await _answerHistoryRepo.GetByIdAsync(historyId);
-        if (history == null) return NotFound($"ID {historyId} の履歴が見つかりません");
-
-        Console.WriteLine(Score);
-
-        // スコアの保存（ここは設計次第で柔軟に）
-        history.Scoring = true;
-        history.Score = Score; // 合計でも平均でもOK
-        history.IsCorrect = Score == 50; // 仮の判定ロジック
-
-        await _answerHistoryRepo.UpdateAsync(history);
-
-        return RedirectToAction("Index"); // 採点後の遷移先
-    }
+    
     
     public static bool HasUnscoredAnswers(string problemId)
     {

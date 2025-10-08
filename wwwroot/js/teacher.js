4666193e34b342f82d259828f7e6d0638dcc80e9
Raikou-56 <raikoukit777@gmail.com>
2025-10-09 00:00:27 +0900

    Update MongoDB config and appsettings

--- wwwroot/js/teacher.js
+++ wwwroot/js/teacher.js
@@ -1,19 +1,18 @@
 $(document).ready(function () {
-    // mathjax即時プレビュー
     const input1 = document.getElementById('latexInput1');
     const preview1 = document.getElementById('mathPreview1');
+    const previewBtn1 = document.getElementById('previewBtn1');
 
-    input1.addEventListener('input', () => {
-        // 入力されたLaTeXをMathJaxで表示
+    previewBtn1.addEventListener('click', () => {
         preview1.innerHTML = `${input1.value}`;
         MathJax.typesetPromise([preview1]);
     });
 
     const input2 = document.getElementById('latexInput2');
     const preview2 = document.getElementById('mathPreview2');
+    const previewBtn2 = document.getElementById('previewBtn2');
 
-    input2.addEventListener('input', () => {
-        // 入力されたLaTeXをMathJaxで表示
+    previewBtn2.addEventListener('click', () => {
         preview2.innerHTML = `${input2.value}`;
         MathJax.typesetPromise([preview2]);
     });

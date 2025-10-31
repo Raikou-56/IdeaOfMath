25d6e37946b90948f81fc069aaca2c95e3f9e474
Raikou-56 <raikoukit777@gmail.com>
2025-10-31 21:59:18 +0900

    Update MongoDB config and appsettings

--- wwwroot/js/editer.js
+++ wwwroot/js/editer.js
@@ -96,7 +96,7 @@ document.getElementById("uploadImageBtn").addEventListener("click", async () => 
             formData.append("problemId", document.querySelector("input[name='SerialNumber']").value);
             formData.append("fileName", altText); // altからファイル名生成
 
-            const response = await fetch("/api/Image/UploadImage", {
+            const response = await fetch("/api/Image/upload", {
                 method: "POST",
                 body: formData
             });

eac2b268f2497aac14c60598a4db863e73e927a1
Raikou-56 <raikoukit777@gmail.com>
2025-12-04 21:59:05 +0900

    Update MongoDB config and appsettings

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,6 @@
 ﻿// layout で読み込み中...
+function setPrintMode(mode) {
+  document.body.classList.remove('print-problem', 'print-all');
+  document.body.classList.add('print-' + mode);
+  window.print();
+}

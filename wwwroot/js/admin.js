789b5a920f1a1cf94ae8ef4a3f9e8459da264c00
Raikou-56 <raikoukit777@gmail.com>
2025-12-08 13:26:39 +0900

    Update MongoDB config and appsettings

--- wwwroot/js/admin.js
+++ wwwroot/js/admin.js
@@ -1,5 +1,5 @@
 $(function() {
-    $(".role-header").on("click", function() {
+    $(".role-bar").on("click", function() {
         var section = $(this).next(".role-users");
         section.slideToggle(); // 押すと展開/折りたたみ
     });

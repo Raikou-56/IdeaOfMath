cb7c5af1ddbc5eca13cf8e35cc5a2c39c21ab858
Raikou-56 <raikoukit777@gmail.com>
2025-12-23 14:32:31 +0900

    Update MongoDB config and appsettings

--- wwwroot/js/admin.js
+++ wwwroot/js/admin.js
@@ -8,6 +8,7 @@ $(function() {
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll(".edit-btn").forEach(btn => {
         btn.addEventListener("click", function () {
+            document.querySelectorAll(".edit-form").forEach(f => f.remove());
             // 既存の選択解除
             document.querySelectorAll(".user-block.selected").forEach(el => {
                 el.classList.remove("selected");
@@ -31,7 +32,15 @@ document.addEventListener("DOMContentLoaded", function () {
                         </div>
                         <div class="modern-input edit-input">
                             <label style="font-weight: bolder;">学年</label>
-                            <input class="modern-control" type="text" name="Grade" value="${block.querySelectorAll('.scor')[1].innerText.replace('学年: ', '')}" />
+                            <select class="modern-control" name="Grade"> 
+                                <option value="">選択してください</option> 
+                                <option value="中学生">中学生</option> 
+                                <option value="高校1年生">高校1年生</option> 
+                                <option value="高校2年生">高校2年生</option> 
+                                <option value="高校3年生">高校3年生</option> 
+                                <option value="浪人生">浪人生</option> 
+                                <option value="大学生以上">大学生以上</option> 
+                            </select>
                         </div>
                         <div class="modern-input edit-input">
                             <label style="font-weight: bolder;">ロール</label>
@@ -43,6 +52,10 @@ document.addEventListener("DOMContentLoaded", function () {
             `;
             block.insertAdjacentHTML("afterend", formHtml);
             let form = block.nextElementSibling;
+
+            const gradeValue = block.querySelectorAll('.scor')[1].innerText.replace('学年: ', ''); 
+            form.querySelector('select[name="Grade"]').value = gradeValue;
+
             requestAnimationFrame(() => {
                 form.classList.add("open");
             });
